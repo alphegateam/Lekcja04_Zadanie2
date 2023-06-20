e4a01ec9efed0f42436548599e4c450112d7f900
manx <manx@DESKTOP-95I9G38>
2023-06-20 21:02:59 +0200

    Add project files.

---
+++ Lekcja04_Zadanie2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+
+namespace Zadanie_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lekcja 4, zadanie dowowe 2.
+            char a, b, c;
+            Console.WriteLine("Podaj pierwszy znak: ");
+            a = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n Drugi pierwszy znak: ");
+            b = Console.ReadKey().KeyChar;
+            Console.WriteLine("\n Trzeci pierwszy znak: ");
+            c = Console.ReadKey().KeyChar;
+            
+            Console.WriteLine("Oto wpisane znaki w odwrotnej kolejności: ");
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+        }
+    }
+}
+
+
